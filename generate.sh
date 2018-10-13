9a18d6060ec116da6d528c417da576f0e99499c9
Calvin Cheng <linchuan.cheng@gmail.com>
2018-10-13 19:20:45 +0800

    Generate golang protobuf

--- generate.sh
+++ generate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+echo "#########################################"
+echo "Generating JavaScript/TypeScript protobuf"
+echo "#########################################"
+
 PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 
 OUT_DIR="jscript/"
@@ -10,5 +15,18 @@ protoc -I=proto/Common -I=proto/Contract -I=proto/Crypto -I=proto/File -I=proto/
     --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
     --js_out=import_style=commonjs,binary:${OUT_DIR} \
     --ts_out=${OUT_DIR} \
+    proto/*/*.proto
+
+echo "##########################"
+echo "Generating golang protobuf"
+echo "##########################"
+
+mkdir -p go
+export GOPATH=$PWD
+export PATH=$PATH:$GOPATH/bin
+go get -v google.golang.org/grpc
+go get -v github.com/golang/protobuf/protoc-gen-go
+protoc -I=proto/Common -I=proto/Contract -I=proto/Crypto -I=proto/File -I=proto/Query -I=proto/Services \
+    --go_out=plugins=grpc:go/ \
     proto/*/*.proto
 
